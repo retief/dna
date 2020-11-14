0b3e75b13711076361e91eebddfc0c3458db4a38
William Zimrin <wzimrin@pop-os.localdomain>
2020-11-13 21:54:22 -0500

    added query to SearchResult

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class SearchResult(models.Model):
     user_id = models.CharField(max_length=40)
+    query = models.CharField(max_length=100)
     protein = models.CharField(max_length=10, blank=True)
     location = models.IntegerField(null=True)
     feature_location = models.CharField(max_length=50, blank=True)
